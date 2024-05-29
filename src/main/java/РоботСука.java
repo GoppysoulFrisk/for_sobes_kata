f1efb7c34e95e84358b8516050436083ef4aa329
GoppysoulFrisk <martakova_aa@list.ru>
2024-05-29 20:09:33 +0300

    ghj

---
+++ src/main/java/РоботСука.java
@@ -0,0 +1,138 @@
+public class РоботСука {
+    public static void main(String[] args) {
+        Robot robot = new Robot(0, 0, Direction.UP);
+        moveRobot(robot, -60, 21);
+    }
+
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public static class Robot {
+        int x;
+        int y;
+        Direction dir;
+
+        public Robot(int x, int y, Direction dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+        }
+
+        public Direction getDirection() {
+            return dir;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void turnLeft() {
+            if (dir == Direction.UP) {
+                dir = Direction.LEFT;
+            } else if (dir == Direction.DOWN) {
+                dir = Direction.RIGHT;
+            } else if (dir == Direction.LEFT) {
+                dir = Direction.DOWN;
+            } else if (dir == Direction.RIGHT) {
+                dir = Direction.UP;
+            }
+        }
+
+        public void turnRight() {
+            if (dir == Direction.UP) {
+                dir = Direction.RIGHT;
+            } else if (dir == Direction.DOWN) {
+                dir = Direction.LEFT;
+            } else if (dir == Direction.LEFT) {
+                dir = Direction.UP;
+            } else if (dir == Direction.RIGHT) {
+                dir = Direction.DOWN;
+            }
+        }
+
+        public void stepForward() {
+            if (dir == Direction.UP) {
+                y++;
+            }
+            if (dir == Direction.DOWN) {
+                y--;
+            }
+            if (dir == Direction.LEFT) {
+                x--;
+            }
+            if (dir == Direction.RIGHT) {
+                x++;
+            }
+        }
+    }
+
+    public static void moveRobot(Robot robot, int toX, int toY) {
+    if (robot.getDirection().equals(Direction.RIGHT)) {
+        System.out.println(robot.dir);
+        robot.turnLeft();
+        System.out.println(robot.dir);
+    } else if (robot.getDirection().equals(Direction.LEFT)) {
+        System.out.println(robot.dir);
+        robot.turnRight();
+        System.out.println(robot.dir);
+    } else if (robot.getDirection().equals(Direction.DOWN)) {
+        System.out.println(robot.dir);
+        robot.turnLeft();
+        robot.turnLeft();
+        System.out.println(robot.dir);
+    }
+
+    if (toX != 0) {
+        if (toX > 0) {
+            robot.turnRight();
+            for (int i = 0; i < toX; i++) {
+                robot.stepForward();
+                System.out.println("x = " + robot.x);
+            }
+        } else {
+            robot.turnLeft();
+            for (int i = 0; i < Math.abs(toX); i++) {
+                robot.stepForward();
+                System.out.println("-x = " + robot.x);
+            }
+        }
+
+        if (toY != 0) {
+            if ((toY > 0) && (toX > 0)) {
+                robot.turnLeft();
+                for (int i = 0; i < toY; i++) {
+                    robot.stepForward();
+                    System.out.println("y = " + robot.y);
+                }
+            } else if ((toY > 0) && (toX < 0)) {
+                robot.turnRight();
+                for (int i = 0; i < toY; i++) {
+                    robot.stepForward();
+                    System.out.println("y = " + robot.y);
+                }
+            } else if ((toY < 0) && (toX > 0)) {
+                robot.turnRight();
+                for (int i = 0; i < Math.abs(toY); i++) {
+                    robot.stepForward();
+                    System.out.println("-y = " + robot.y);
+                }
+            } else if ((toY < 0) && (toX < 0)) {
+                robot.turnLeft();
+                for (int i = 0; i < Math.abs(toY); i++) {
+                    robot.stepForward();
+                    System.out.println("-y = " + robot.y);
+                }
+            }
+
+        }
+    }
+    }
+}
